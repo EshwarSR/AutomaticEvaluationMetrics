eefd954866445a66b52f46ff917a83a3b3b1c6a2
codewithkaranjeswani <krnjeswani21@gmail.com>
2020-04-18 14:52:17 +0530

    Changing main to run on updated data

--- 1_BERTScore/mainz.py
+++ 1_BERTScore/mainz.py
@@ -2,22 +2,22 @@ import pandas as pd
 import numpy as np
 import os
 from bert_score import score
-lc = 100
-lr = 441
 
-# 1st 100 lines are cands, next 441 lines are refs
-file_name = '../data/ess3_small.txt'
-df = pd.read_csv(file_name, delimiter='\n', header=None)
-print("Total Sentences: ", len(df))
+fn1 = '../data/cand_100.tsv'
+fn2 = '../data/reference_data.tsv'
+df1 = pd.read_csv(fn1, delimiter='\t')
+df2 = pd.read_csv(fn2, delimiter='\t')
 
-cands = df[:100].values.tolist()
-print(len(cands))
+cands = df1.values.tolist()
+print("Candidate Sentences: ", len(cands))
+refs = df2.values.tolist()
+print("Reference Sentences: ", len(refs))
 
-refs = df[100:].values.tolist()
-refs = [refs]  # * len(cands)
-print(len(refs))
+# broadcasting refs so that len(cands) == len(refs)
+refs = [refs] * len(cands)
+assert len(cands) == len(refs)
 
-sol = score(cands, refs, model_type=None, num_layers=None, verbose=False,
+sol = score(cands, refs, model_type=None, num_layers=None, verbose=True,
             idf=False, device=None, batch_size=64, nthreads=4, all_layers=False,
             lang="en", return_hash=False, rescale_with_baseline=False)
 
