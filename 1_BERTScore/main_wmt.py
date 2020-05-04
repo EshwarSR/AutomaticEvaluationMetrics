10a7f19ed1a59ac817b74441b3882fa2c09e47b0
codewithkaranjeswani <krnjeswani21@gmail.com>
2020-05-04 16:11:44 +0530

    Better code in main_wmt.py

--- 1_BERTScore/main_wmt.py
+++ 1_BERTScore/main_wmt.py
@@ -11,14 +11,16 @@ allrefs = '../data/WMT18Data/references/'
 human = '../data/WMT18Data/DA-syslevel.csv'
 
 act_dir = []
+todo = []
 cdirs = os.listdir(allcees)
 for csdir in cdirs:
 	if csdir[-2:] == 'en':
+		todo += [csdir]
 		act_dir += [allcees + csdir]
 
 for csdir in act_dir:
 	stry = csdir[-5:-3] + csdir[-2:]
-	rs = allrefs + '/newstest2018-' + stry + '-ref.en'
+	rs = allrefs + 'newstest2018-' + stry + '-ref.en'
 	cses = os.listdir(csdir)
 
 	refs = []
@@ -31,7 +33,7 @@ for csdir in act_dir:
 
 	outlist = []
 	for cs in cses:
-		lp = csdir[-6:-1]
+		lp = csdir[-5:]
 		sys = cs[13:-6]
 		hdf = pd.read_csv(human, sep=' ')
 
@@ -40,7 +42,7 @@ for csdir in act_dir:
 		hdf.reset_index(drop=True, inplace=True)
 
 		cands = []
-		fc = open(csdir + cs, "r", encoding='utf-8')
+		fc = open(csdir + '/' + cs, "r", encoding='utf-8')
 		while True:
 			line = fc.readline()
 			if not line:
@@ -53,18 +55,31 @@ for csdir in act_dir:
 		            idf=True, device=None, batch_size=64, nthreads=4, all_layers=False,
 		            lang="en", return_hash=False, rescale_with_baseline=True)
 
-		outlist += [[P.mean().item(), R.mean().item(), F1.mean().item(), hdf['HUMAN'].item()]]
+		outlist.append([P.mean().item(), R.mean().item(), F1.mean().item(), hdf['HUMAN'].item()])
 
 	out = pd.DataFrame(outlist, columns = ["P", "R", "F1", "H"])
-	print(out)
-	out.to_csv("scores" + stry + ".tsv", sep="\t", index=False, header=True)
+	# print(out)
+	out.to_csv("scores-" + stry + ".tsv", sep="\t", index=False, header=True)
 
 	cp = spearmanr(out['P'], out['H'])
 	cr = spearmanr(out['R'], out['H'])
 	cf1 = spearmanr(out['F1'], out['H'])
-	pearson_corrlist = [[cp.correlation, cp.pvalue, cr.correlation, cr.pvalue, cr.correlation, cr.pvalue]]
+	spearman_corrlist = [['Spearman Rank Correlation', cp.correlation, cp.pvalue, cr.correlation, cr.pvalue, cf1.correlation, cf1.pvalue]]
 
-	pearson_corr = pd.DataFrame(pearson_corrlist, columns = ["P", "P-pval", "R", "R-pval", "F1", "F1-pval"])
-	print(pearson_corr)
-	pearson_corr.to_csv("corr" + stry + ".tsv", sep="\t", index=False, header=True)
-	break
+	cp = pearsonr(out['P'], out['H'])
+	cr = pearsonr(out['R'], out['H'])
+	cf1 = pearsonr(out['F1'], out['H'])
+	pearson_corrlist = [['Pearson Correlation Coefficient', cp[0], cp[1], cr[0], cr[1], cf1[0], cf1[1]]]
+
+	cp = kendalltau(out['P'], out['H'])
+	cr = kendalltau(out['R'], out['H'])
+	cf1 = kendalltau(out['F1'], out['H'])
+	kend_corrlist = [['Kendall Rank Correlation', cp[0], cp[1], cr[0], cr[1], cf1[0], cf1[1]]]
+
+	fin_list = spearman_corrlist + pearson_corrlist + kend_corrlist
+
+	corrs = pd.DataFrame(fin_list, columns = ["Type of Correlation", "P", "P-pval", "R", "R-pval", "F1", "F1-pval"])
+	# print(corrs)
+	corrs.to_csv("corr-" + stry + ".tsv", sep="\t", index=False, header=True)
+	print(stry + "done")
+	print(todo)
