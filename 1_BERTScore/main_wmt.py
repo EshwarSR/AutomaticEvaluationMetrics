76d1c714e8c4782584e689d15fc97446e445f0bf
codewithkaranjeswani <krnjeswani21@gmail.com>
2020-05-04 12:34:01 +0530

    Testing the main_wmt.py

--- 1_BERTScore/main_wmt.py
+++ 1_BERTScore/main_wmt.py
@@ -3,55 +3,68 @@ import sys
 import pandas as pd
 import numpy as np
 import os
-import torch
-from bert_score import score
+# import torch
+# from bert_score import score
 
-csdir = '../data/WMT18Data/system-outputs/newstest2018/cs-en/'
-cses = os.listdir(csdir)
-rs = '../data/WMT18Data/references/newstest2018-csen-ref.en'
+allcees = '../data/WMT18Data/system-outputs/newstest2018/'
+allrefs = '../data/WMT18Data/references/'
 human = '../data/WMT18Data/DA-syslevel.csv'
 
-refs = []
-fr = open(rs, "r", encoding='utf-8')
-while True:
-	line = fr.readline() 
-	if not line:
-		break
-	refs.append(line)
+act_dir = []
+cdirs = os.listdir(allcees)
+for csdir in cdirs:
+	if csdir[-2:] == 'en':
+		act_dir += [allcees + csdir]
 
-outlist = []
-for cs in cses:
-	lp = csdir[-6:-1]
-	sys = cs[13:-6]
-	hdf = pd.read_csv(human, sep=' ')
+for csdir in act_dir:
+	stry = csdir[-5:-3] + csdir[-2:]
+	rs = allrefs + 'newstest2018-' + stry + '-ref.en'
+	cses = os.listdir(csdir)
 
-	hdf = hdf.loc[hdf['LP'] == lp]
-	hdf = hdf.loc[hdf['SYSTEM'] == sys]
-	hdf.reset_index(drop=True, inplace=True)
-
-	cands = []
-	fc = open(csdir + cs, "r", encoding='utf-8')
+	refs = []
+	fr = open(rs, "r", encoding='utf-8')
 	while True:
-		line = fc.readline()
+		line = fr.readline() 
 		if not line:
 			break
-		cands.append(line)
+		refs.append(line)
 
-	assert len(cands) == len(refs)
+	outlist = []
+	for cs in cses:
+		lp = csdir[-6:-1]
+		sys = cs[13:-6]
+		hdf = pd.read_csv(human, sep=' ')
 
-	P, R, F1 = score(cands, refs, model_type=None, num_layers=None, verbose=False,
-	            idf=True, device=None, batch_size=64, nthreads=4, all_layers=False,
-	            lang="en", return_hash=False, rescale_with_baseline=True)
+		hdf = hdf.loc[hdf['LP'] == lp]
+		hdf = hdf.loc[hdf['SYSTEM'] == sys]
+		hdf.reset_index(drop=True, inplace=True)
 
-	outlist += [[P.mean().item(), R.mean().item(), F1.mean().item(), hdf['HUMAN'].item()]]
+		cands = []
+		fc = open(csdir + cs, "r", encoding='utf-8')
+		while True:
+			line = fc.readline()
+			if not line:
+				break
+			cands.append(line)
 
-out = pd.DataFrame(outlist, columns = ["P", "R", "F1", "H"])
-print(out)
+		assert len(cands) == len(refs)
 
-cp = spearmanr(out['P'], out['H'])
-cr = spearmanr(out['R'], out['H'])
-cf1 = spearmanr(out['F1'], out['H'])
-pearson_corrlist = [[cp.correlation, cp.pvalue, cr.correlation, cr.pvalue, cr.correlation, cr.pvalue]]
+		P, R, F1 = score(cands, refs, model_type=None, num_layers=None, verbose=False,
+		            idf=True, device=None, batch_size=64, nthreads=4, all_layers=False,
+		            lang="en", return_hash=False, rescale_with_baseline=True)
 
-pearson_corr = pd.DataFrame(pearson_corrlist, columns = ["P", "P-pval", "R", "R-pval", "F1", "F1-pval"])
-print(pearson_corr)
+		outlist += [[P.mean().item(), R.mean().item(), F1.mean().item(), hdf['HUMAN'].item()]]
+
+	out = pd.DataFrame(outlist, columns = ["P", "R", "F1", "H"])
+	print(out)
+	out.to_csv("scores" + stry + ".tsv", sep="\t", index=False, header=True)
+
+	cp = spearmanr(out['P'], out['H'])
+	cr = spearmanr(out['R'], out['H'])
+	cf1 = spearmanr(out['F1'], out['H'])
+	pearson_corrlist = [[cp.correlation, cp.pvalue, cr.correlation, cr.pvalue, cr.correlation, cr.pvalue]]
+
+	pearson_corr = pd.DataFrame(pearson_corrlist, columns = ["P", "P-pval", "R", "R-pval", "F1", "F1-pval"])
+	print(pearson_corr)
+	pearson_corr.to_csv("corr" + stry + ".tsv", sep="\t", index=False, header=True)
+	break
