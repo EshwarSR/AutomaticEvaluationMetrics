5f24aa53ff457bed0ea5803faacbec4ad43fe1b3
EshwarSR <sr.eshwar07@gmail.com>
2020-05-07 13:37:39 +0530

    Results

--- 3_SMS/job_elmo_s+wms.sh
+++ 3_SMS/job_elmo_s+wms.sh
@@ -10,6 +10,6 @@
 #source /home/eshwarsr/IISc-ML-Project/virtual_env/bin/activate
 
 conda activate base
-python -u driver_wmt.py ../data/WMT18Data/system-outputs/newstest2018/et-en/ ../data/WMT18Data/references/newstest2018-eten-ref.en elmo s+wms > logs/redirect_elmo_wmt_et_s+wms_`date +%d_%m_%Y_%H_%M_%S`.log 2>&1
+python -u driver_wmt.py ../data/WMT18Data/system-outputs/newstest2018/ru-en/ ../data/WMT18Data/references/newstest2018-ruen-ref.en elmo s+wms > logs/redirect_elmo_wmt_ru_s+wms_`date +%d_%m_%Y_%H_%M_%S`.log 2>&1
 
 echo "Done"
