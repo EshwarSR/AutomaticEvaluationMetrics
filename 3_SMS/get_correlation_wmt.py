603adbc26a95bfbb3d9ed74c093509e64701347b
EshwarSR <sr.eshwar07@gmail.com>
2020-05-10 18:42:52 +0530

    Generating correlations and p values

--- 3_SMS/get_correlation_wmt.py
+++ 3_SMS/get_correlation_wmt.py
@@ -2,8 +2,8 @@ from scipy.stats import spearmanr, pearsonr, kendalltau
 import sys
 import pandas as pd
 
-results_file = "../results/WMT_EMD_metrics.csv"
-data = pd.read_csv(results_file)
+results_file = "../results/WMT18_Results/final/scores_all.tsv"
+data = pd.read_csv(results_file, sep="\t")
 
 metrics = list(data.columns)
 metrics.remove("HUMAN")
@@ -13,6 +13,7 @@ metrics.remove("LP")
 correlations = ["Spearman", "Pearson", "Kendall"]
 
 final_correlations = []
+final_p_values = []
 
 groups = data.groupby("LP")
 for metric in metrics:
@@ -22,59 +23,28 @@ for metric in metrics:
             "metric": metric,
             "correlation": correlation,
         }
+        p_row = {
+            "metric": metric,
+            "correlation": correlation,
+        }
         for language, group in groups:
             if correlation == "Spearman":
-                corr = spearmanr(group[metric], group["HUMAN"]).correlation
+                resp = spearmanr(group[metric], group["HUMAN"])
+                corr = resp.correlation
+                p_val = resp.pvalue
             elif correlation == "Pearson":
-                corr = pearsonr(group[metric], group["HUMAN"])[0]
+                corr, p_val = pearsonr(group[metric], group["HUMAN"])
             elif correlation == "Kendall":
-                corr = kendalltau(group[metric], group["HUMAN"])[0]
+                corr, p_val = kendalltau(group[metric], group["HUMAN"])
             row[language] = corr
+            p_row[language] = p_val
         final_correlations.append(row)
+        final_p_values.append(p_row)
 
 final_df = pd.DataFrame(final_correlations)
+final_p_df = pd.DataFrame(final_p_values)
 print("Saving to file")
-final_df.to_csv("../results/WMT_EMD_correlations.csv", index=False)
-
-# if len(sys.argv) > 3:
-#     results_file = sys.argv[1]
-#     field = sys.argv[2]
-#     agg = sys.argv[3]
-#     results = pd.read_csv(results_file, sep="\t")
-#     groups = results.groupby("candidate_id")
-#     similarity = []
-#     score = []
-#     for candidate_id, group in groups:
-#         score.append(group["score"].mean())
-#         if agg == "mean" or agg == "avg":
-#             similarity.append(group[field].mean())
-#         elif agg == "max":
-#             similarity.append(group[field].max())
-
-# else:
-#     results_file = sys.argv[1]
-#     field = sys.argv[2]
-#     results = pd.read_csv(results_file, sep="\t")
-#     similarity = results[field].tolist()
-#     score = results["score"].tolist()
-
-
-# scorr = spearmanr(similarity, score)
-# # print("File:", results_file)
-# print("Spearman Rank Correlation:{0:6.3f}".format(scorr.correlation))
-# if (scorr.pvalue >= 0.001):
-#     print("P-value:{0:6.3f}".format(scorr.pvalue))
-# else:
-#     print("P-value:{0:10.3e}".format(scorr.pvalue))
-# pcorr = pearsonr(similarity, score)
-# print("Pearson Correlation Coefficient:{0:6.3f}".format(pcorr[0]))
-# if (pcorr[1] >= 0.001):
-#     print("P-value:{0:6.3f}".format(pcorr[1]))
-# else:
-#     print("P-value:{0:10.3e}".format(pcorr[1]))
-# kentau = kendalltau(similarity, score)
-# print("Kendall Rank Correlation :{0:6.3f}".format(kentau[0]))
-# if (kentau[1] >= 0.001):
-#     print("P-value:{0:6.3f}".format(kentau[1]))
-# else:
-#     print("P-value:{0:10.3e}".format(kentau[1]))
+final_df.to_csv("../results/WMT18_Results/final/all_correlations.tsv",
+                index=False, sep="\t")
+final_p_df.to_csv(
+    "../results/WMT18_Results/final/all_pvalues.tsv", index=False, sep="\t")
