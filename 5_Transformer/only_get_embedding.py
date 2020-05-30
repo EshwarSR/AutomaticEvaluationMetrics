63b771ffa499ed3f6d82071a0fc1b7b2e6c4684d
codewithkaranjeswani <krnjeswani21@gmail.com>
2020-05-30 15:07:20 +0530

    Adding test file

---
+++ 5_Transformer/only_get_embedding.py
@@ -0,0 +1,19 @@
+from transformers import AutoTokenizer, AutoModels
+
+model_name = 'roberta-large'
+num_layers = 17
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModel.from_pretrained(model_name)
+
+assert (0 <= num_layers <= len(model.encoder.layer)), f"Invalid num_layers: num_layers should be between 0 and {len(model.encoder.layer)} for {model_name}"
+model.encoder.layer = torch.nn.ModuleList([layer for layer in model.encoder.layer[:num_layers]])
+
+onesent = 'Some sentence is here.'
+onesent_tokens = tokenizer.encode(torch.tensor([onsent]))
+
+model.eval()
+with torch.no_grad():
+  out = model(onesent_tokens, attention_mask=attention_mask)
+  emb = out[0]
+return emb
