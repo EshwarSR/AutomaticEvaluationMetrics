a568e999b20d89c4ef226894a25e6820c278fe49
Rahul <rjroy196@gmail.com>
2020-05-30 18:54:30 +0530

    Adding code for sentence transformer

--- 5_Transformer/only_get_embedding.py
+++ 5_Transformer/only_get_embedding.py
@@ -17,3 +17,13 @@ with torch.no_grad():
   out = model(onesent_tokens, attention_mask=attention_mask)
   emb = out[0]
 return emb
+
+### Code for sentencetransformers
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('bert-large-nli-stsb-mean-tokens')
+
+sentences = ['This framework generates embeddings for each input sentence',
+    'Sentences are passed as a list of string.', 
+    'The quick brown fox jumps over the lazy dog.']
+sentence_embeddings = model.encode(sentences)
