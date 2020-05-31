290072aef114b1f25d4c17432a1f0ae7f027ca97
EshwarSR <sr.eshwar07@gmail.com>
2020-05-31 13:45:59 +0530

    Run roberta on SAS

--- 3_SMS/driver_sms.py
+++ 3_SMS/driver_sms.py
@@ -97,28 +97,147 @@ def process_dataset(dataset, results_file_name):
     print("Finished writing file")
 
 
+def get_all_ref_emb_weights():
+    reference_data = pd.read_csv(REFERENCE_FILE, sep="\t")
+    print("Number of references:", len(reference_data))
+    reference_data = reference_data.to_dict("records")
+    processed_refs = {}
+    next_id = 0
+    emb = {}
+    for item in reference_data:
+        doc = item[essay_field]
+        next_id, emb, id_list, weights = get_embeddings_ids_weights(
+            doc, next_id, emb)
+        processed_refs[item[id_field]] = {
+            "id_list": id_list,
+            "weights": weights
+        }
+
+    return processed_refs, next_id, emb
+
+
+def calculate_multireference_similarity(candidate, processed_refs, next_id, emb):
+    s = time.time()
+    can_doc = nlp(candidate[essay_field])
+    similarities = []
+    next_id, emb, can_id_list, can_weights = get_embeddings_ids_weights(
+        can_doc, next_id, emb)
+    nbow = {
+        "hypothesis": ("hypothesis", can_id_list, can_weights)
+    }
+
+    for id, item in processed_refs.items():
+        ref_weights = item["weights"]
+        ref_id_list = item["id_list"]
+        nbow[id] = (id, ref_id_list, ref_weights)
+
+    calc = WMD(emb, nbow, vocabulary_min=1)
+    # print("NBOW")
+    # print(nbow)
+    distances = calc.nearest_neighbors(
+        "hypothesis", k=len(processed_refs), early_stop=1)
+
+    for id, dist in distances:
+        similarity = np.exp(-dist)
+        similarities.append({
+            "candidate_id": candidate[id_field],
+            "reference_id": id,
+            "similarity": similarity,
+            "dist": dist,
+            "score": candidate[score_field]
+        })
+    print("Time taken for candidate " +
+          str(candidate[id_field]) + " is " + str(time.time() - s))
+
+    return similarities
+
+
+def process_multireference_dataset(candidates, processed_reference, results_file_name):
+    # Dataset should have candidate,reference and score
+    st = time.time()
+    processed_refs, next_id, emb = get_all_ref_emb_weights()
+    print("Done preprocessing references", time.time() - st)
+    final_results = []
+    idx = 0
+
+    # for idx, candidate in enumerate(candidates_data):
+    while idx < len(candidates):
+        candidate = candidates[idx]
+        try:
+            resp = calculate_multireference_similarity(
+                candidate, processed_refs, next_id, emb)
+            final_results.extend(resp)
+        except:
+            print("ERROR WHILE PROCESSING:", candidate[id_field])
+
+        if idx % 100 == 0:
+            final_results_df = pd.DataFrame(final_results)
+            final_results_df.to_csv(
+                results_file_name, sep="\t", index=False)
+        idx += 1
+
+    print("Done with similarity")
+    final_results_df = pd.DataFrame(final_results)
+    print("Created Dataframe")
+    print(final_results_df)
+    final_results_df.to_csv(results_file_name, sep="\t", index=False)
+    print("Finished writing file")
+
+
 #################
 # Main Function #
 #################
-
 st = time.time()
 
 # CNN MailDataset
-DATASET_FILE = "../data/CNN_DailyMail.tsv"
-print("Reading dataset from", DATASET_FILE)
-dataset = pd.read_csv(DATASET_FILE, sep="\t").to_dict("records")
+# DATASET_FILE = "../data/CNN_DailyMail.tsv"
+# print("Reading dataset from", DATASET_FILE)
+# dataset = pd.read_csv(DATASET_FILE, sep="\t").to_dict("records")
 
-nlp = spacy.load("en_core_web_md")
-model = SentenceTransformer('bert-large-nli-stsb-mean-tokens')
-print("Loaded model", time.time() - st)
+# nlp = spacy.load("en_core_web_md")
+# model = SentenceTransformer('bert-large-nli-stsb-mean-tokens')
+# print("Loaded model", time.time() - st)
 
-results_file_name = "../results/SentBERT/CNN_DailyMail_SentBERT.tsv"
-process_dataset(dataset, results_file_name)
+# results_file_name = "../results/SentBERT/CNN_DailyMail_SentBERT.tsv"
+# process_dataset(dataset, results_file_name)
 
 
-# Essay Dataset
-REFERENCE_FILE = "../data/ASAP_AES/aes_reference_data.tsv"
-CANDIDATES_FILE = "../data/ASAP_AES/aes_candidates_data.tsv"
+# ASAP AES Dataset
+# REFERENCE_FILE = "../data/ASAP_AES/aes_reference_data.tsv"
+# CANDIDATES_FILE = "../data/ASAP_AES/aes_candidates_data.tsv"
+# print("Reading dataset from", REFERENCE_FILE, CANDIDATES_FILE)
+
+# candidates_data = pd.read_csv(CANDIDATES_FILE, sep="\t")
+# print("Number of candidates:", len(candidates_data))
+# candidates_data = candidates_data.to_dict("records")
+
+# reference_data = pd.read_csv(REFERENCE_FILE, sep="\t")
+# print("Number of references:", len(reference_data))
+# reference_data = reference_data.to_dict("records")
+
+# score_field = "domain1_score"
+# essay_field = "essay"
+# id_field = "essay_id"
+
+# processed_dataset = []
+# for cand in candidates_data:
+#     for ref in reference_data:
+#         sample = {
+#             "candidate": cand[essay_field],
+#             "candidate_id": cand[id_field],
+#             "reference": ref[essay_field],
+#             "reference_id": ref[id_field],
+#             "score": cand[score_field]
+#         }
+#         processed_dataset.append(sample)
+
+# results_file_name = "../results/SentBERT/ASAP_AES_SentBERT.tsv"
+# process_dataset(processed_dataset, results_file_name)
+
+
+# ASAP SAS Dataset
+REFERENCE_FILE = "../data/ASAP_SAS/reference_data.tsv"
+CANDIDATES_FILE = "../data/ASAP_SAS/candidates_data.tsv"
 print("Reading dataset from", REFERENCE_FILE, CANDIDATES_FILE)
 
 candidates_data = pd.read_csv(CANDIDATES_FILE, sep="\t")
@@ -129,21 +248,10 @@ reference_data = pd.read_csv(REFERENCE_FILE, sep="\t")
 print("Number of references:", len(reference_data))
 reference_data = reference_data.to_dict("records")
 
-score_field = "domain1_score"
-essay_field = "essay"
-id_field = "essay_id"
+score_field = "Score1"
+essay_field = "EssayText"
+id_field = "Id"
 
-processed_dataset = []
-for cand in candidates_data:
-    for ref in reference_data:
-        sample = {
-            "candidate": cand[essay_field],
-            "candidate_id": cand[id_field],
-            "reference": ref[essay_field],
-            "reference_id": ref[id_field],
-            "score": cand[score_field]
-        }
-        processed_dataset.append(sample)
-
-results_file_name = "../results/SentBERT/ASAP_AES_SentBERT.tsv"
-process_dataset(processed_dataset, results_file_name)
+results_file_name = "../results/SentBERT/ASAP_SAS_SentBERT.tsv"
+process_multireference_dataset(
+    candidates_data, reference_data, results_file_name)
--- 3_SMS/run_all.sh
+++ 3_SMS/run_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-python3 -u driver.py roberta-large wms > logs/redirect_roberta_aes_wms_`date +%d_%m_%Y_%H_%M_%S`.log 2>&1
-python3 -u driver.py roberta-large sms > logs/redirect_roberta_aes_sms_`date +%d_%m_%Y_%H_%M_%S`.log 2>&1
-python3 -u driver.py roberta-large s+wms > logs/redirect_roberta_aes_s+wms_`date +%d_%m_%Y_%H_%M_%S`.log 2>&1
+
+python3 -u driver.py roberta-large wms > logs/redirect_roberta_sas_wms_`date +%d_%m_%Y_%H_%M_%S`.log 2>&1
+echo 'Done with Roberta sas wms'
+python3 -u driver.py roberta-large sms > logs/redirect_roberta_sas_sms_`date +%d_%m_%Y_%H_%M_%S`.log 2>&1
+echo 'Done with Roberta sas sms'
+python3 -u driver.py roberta-large s+wms > logs/redirect_roberta_sas_s+wms_`date +%d_%m_%Y_%H_%M_%S`.log 2>&1
+echo 'Done with Roberta sas s+wms'
+
+python3 -u driver_sms.py > logs/redirect_sentbert_sas_sms_`date +%d_%m_%Y_%H_%M_%S`.log 2>&1
+echo 'Done with SentBERT sas sms'
